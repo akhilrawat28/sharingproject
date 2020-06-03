13a3cdcbf7031e600ea4f60a695c4d6c2c650dd5
akhilrawat28 <64654040+akhilrawat28@users.noreply.github.com>
2020-06-03 16:22:29 +0530

    Add files via upload
    Python dictionary is for normal virtual dictionary.
    in this program i have  use get_close_matches() function by importing difflib package.

---
+++ dict.py
@@ -0,0 +1,31 @@
+import json
+
+data =  json.load(open("original.json"))
+
+def translate(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif word.title() in data:
+        return data[word.title()]
+    elif word.upper() in data:
+        return data[word.upper()]
+    elif len(get_close_matches(word , data.keys())) > 0:
+        print("did you mean %s instead" %get_close_matches(word, data.keys())[0])
+        decide = input("press y for yes and n for no")
+        if decide == "y":
+                return data[get_close_matches(word , data.keys())[0]]
+        elif decide ==n :
+            return("word not found")
+        else :
+            retun("not found")
+    else:
+        print("word not found")
+
+word = input("ENTER THE WORD YOU WANT TO SEARCH")
+output = translate(word)
+if type(output)==list:
+    for item in output:
+        print(item)
+else:
+    print(output)
